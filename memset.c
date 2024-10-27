6d9c8b9ef561c0f63f6fc56673c1393d819bfefd
lucas-koji <furukawa.lucaskoji@gmail.com>
2024-10-27 09:28:15 -0300

    feat: add memset

---
+++ memset.c
@@ -0,0 +1,16 @@
+#include "clib.h"
+
+void	*memset(void *s, int c, size_t n)
+{
+	size_t	i;
+	char	*str;
+
+	str = (char *) s;
+	i = 0;
+	while (i < n)
+	{
+		str[i] = c;
+		i++;
+	}
+	return (s);
+}
